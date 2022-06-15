623bacc3c372237ba2c3692f23779f26313f5737
rames3 <ramesh.jamakatel@cogenthealth.com.np>
2022-06-15 22:26:27 +0545

    new-project

---
+++ src/test/java/net/bloodbank/BloodBankApplicationTests.java
@@ -0,0 +1,13 @@
+package net.bloodbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
